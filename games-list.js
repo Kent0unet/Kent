98097de330b47a04d8331836334e9ff981dacf97
Kent <kent9prod@gmail.com>
2026-02-09 23:09:18 +0100

    Update games-list.js

--- games-list.js
+++ games-list.js
@@ -148,7 +148,7 @@ window.GAMES = [
       { title: 'Duet Night Abyss', url: 'games/duet-night-abyss.html' },
       { title: 'Dispatch', url: 'games/dispatch.html' },
       { title: 'Dinkum', url: null },
-      { title: 'Arknights : Endfield', url: null },
+      { title: 'Arknights : Endfield', url: 'games/arknights-endfield.html' },
       { title: 'Hytale', url: null },
       { title: 'Starsand Island', url: null },
       { title: 'Tainted Grail : The Fall of Avalon', url: null },
